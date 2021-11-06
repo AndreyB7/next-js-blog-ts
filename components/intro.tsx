74f7976e9a5cec4cf80f79189303ec65b722c554
AndreyB7 <0129507@gmail.com>
2021-11-06 11:36:47 +0700

    test build

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,6 @@ const Intro = () => {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <div className="text-xs text-green-500">updated with commit to main</div>
       <nav>
         <Link href="/about">Link to new About page</Link>
       </nav>
